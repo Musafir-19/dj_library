1cf3bb9983ba9c47a5aff73b9be1b4d01882c1f4
Musafir-19 <rkhunkarov@mail.ru>
2022-01-23 22:26:10 +0300

    search/Api

---
+++ README.md
@@ -0,0 +1,18 @@
+## Библиотека
+
+### На сайте реализовано:
+#### - Регистрация
+#### - Логин и логаут
+#### - База о наличии физических книг
+#### - Поиск, сначала в базе, а потом через стороннее API
+
+### Установите Python версии 3.6 и выше
+#### Зависимости
+
+#### Установите зависимости из файла requirements.txt
+#### Команда для Windows `pip install -r requirements.txt`
+#### Команда для MacOS и Linux `pip3 install -r requirements.txt`
+
+### Запуск 
+#### Для запуска на Windows `python .\manage.py runserver`
+#### Для запуска на MacOs или Linix `python3 .\manage.py runserver`
--- search/views.py
+++ search/views.py
@@ -1,16 +1,26 @@
 import requests
 from django.shortcuts import render
+from books.models import Book
 
 
 def search_page(request):
     query = request.POST.get("search_field")
-    res = requests.get(
-        f'https://www.googleapis.com/books/v1/volumes?q={query}').json()
-    for value in res["items"]:
-        book_title = value["volumeInfo"]["title"]
-        book_author = value["volumeInfo"]["authors"][0]
-        date = value["volumeInfo"]["publishedDate"]
-        datadict = {'title': book_title, 'author': book_author,
-                    'date': date}
-        print(datadict)
-    return render(request, 'search/results.html', {'datadict': datadict})
+    if  Book.objects.filter(title__contains=query):
+        books = Book.objects.filter(title__contains=query)
+    else:
+        res = requests.get(
+            f'https://www.googleapis.com/books/v1/volumes?q={query}').json()
+        books = []
+        for value in res["items"]:
+            url = value["volumeInfo"]["previewLink"]
+            book_title = value["volumeInfo"]["title"]
+            try:
+                book_author = value["volumeInfo"]["authors"][0]
+                date = value["volumeInfo"]["publishedDate"]
+            except:
+                book_author = "Нет данных"
+            datadict = {'title': book_title, 'author': book_author,
+                        'date': date, 'url': url}
+            books.append(datadict)
+            print(books)
+    return render(request, 'search/results.html', {'datadict': books})
